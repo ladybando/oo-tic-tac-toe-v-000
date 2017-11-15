52b39ada291b1605ec62ba5cc5cf4e7e3ebb069d
Ayana Bando <ayanaflax@yahoo.com>
2017-11-15 02:50:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,12 +37,23 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
 
-  def won?
-     WIN_COMBINATIONS.detect do |win|
-       @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]] && position_taken?(win[0])
+  def won?(board = @board)
+   WIN_COMBINATIONS.detect do |win_combination|
+     win_index_1 = win_combination[0]
+     win_index_2 = win_combination[1]
+     win_index_3 = win_combination[2]
+
+     position_1 = board[win_index_1]
+     position_2 = board[win_index_2]
+     position_3 = board[win_index_3]
+
+     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+       return win_combination
+     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+       return win_combination
      end
    end
-
+ end
 def full?
   @board.all?{|token| token == "X" || token == "O"}
 end
